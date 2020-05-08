e52145e1b3f5873d9cb7538ea5a20226bb7a0968
Aaron <cao_shuai688@163.com>
2020-05-08 19:57:44 +0800

    springmvc的简单web项目的解读

--- src/main/java/com/aaron/controller/HelloController.java
+++ src/main/java/com/aaron/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Controller处理器的编写
+ */
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
